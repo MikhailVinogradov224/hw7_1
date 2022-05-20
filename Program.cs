e83148e37fc10fcde8219a94bf50ae7bdeb91508
Mikhail Vinogradov <m89119750476@gmail.com>
2022-05-21 00:11:26 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int m = 3;
+int n =4;
+double[,] a = new double[m, n];
+
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int k = 0; k < n; k++)
+    {
+        a[i, k] = random.NextDouble() * 100;
+        Console.Write("{0,6:F2}", a[i, k]);
+    }
+    Console.WriteLine();
+}
